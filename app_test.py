575a23662e677a4c75a72e570d7fe4a6795c125a
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-05-25 14:42:39 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,7 +3,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # Open a file dialog box to select the Excel file
-file_path = st.file_uploader("Upload Excel file", type=["xls", "xlsx"])
+file_path = st.file_uploader("Upload Excel file", type=["xls", "xlsx"], key="excel_uploader")
 
 if file_path is not None:
     # Read the Excel file into a DataFrame
@@ -51,60 +51,6 @@ if file_path is not None:
     class_b_percentage = (length_class_b / total_hours) * 100
     class_c_percentage = (length_class_c / total_hours) * 100
 
-import pandas as pd
-import streamlit as st
-import matplotlib.pyplot as plt
-
-# Open a file dialog box to select the Excel file
-file_path = st.file_uploader("Upload Excel file", type=["xls", "xlsx"])
-
-if file_path is not None:
-    # Read the Excel file into a DataFrame
-    df = pd.read_excel(file_path)
-    df.columns = ["outside_temperature", "inside_temperature"]
-
-    def class_a(inside_temperature, outside_temperature):
-        lower_limit = max(18.8 - 2 + 0.33 * outside_temperature, 21)
-        upper_limit = max(18.8 + 2 + 0.33 * outside_temperature, 23)
-        if inside_temperature >= lower_limit and inside_temperature <= upper_limit:
-            return 1
-        else:
-            return 0
-
-    df["class_A"] = df.apply(lambda x: class_a(x["inside_temperature"], x["outside_temperature"]), axis=1)
-
-    def class_b(inside_temperature, outside_temperature):
-        lower_limit = max(18.8 - 3 + 0.33 * outside_temperature, 20)
-        upper_limit = max(18.8 + 3 + 0.33 * outside_temperature, 24)
-        if inside_temperature >= lower_limit and inside_temperature <= upper_limit:
-            return 1
-        else:
-            return 0
-
-    df["class_B"] = df.apply(lambda x: class_b(x["inside_temperature"], x["outside_temperature"]), axis=1)
-
-    def class_c(inside_temperature, outside_temperature):
-        lower_limit = max(18.8 - 4 + 0.33 * outside_temperature, 19)
-        upper_limit = max(18.8 + 4 + 0.33 * outside_temperature, 25)
-        if inside_temperature >= lower_limit and inside_temperature <= upper_limit:
-            return 1
-        else:
-            return 0
-
-    df["class_C"] = df.apply(lambda x: class_c(x["inside_temperature"], x["outside_temperature"]), axis=1)
-
-    total_hours = len(df["class_C"])
-    length_class_a = len(df[df["class_A"] == 1])
-    length_class_b = len(df[df["class_B"] == 1])
-    length_class_c = len(df[df["class_C"] == 1])
-
-    discomfort_percentage = ((total_hours - length_class_c) / total_hours) * 100
-    comfort_percentage = (length_class_c / total_hours) * 100
-    class_a_percentage = (length_class_a / total_hours) * 100
-    class_b_percentage = (length_class_b / total_hours) * 100
-    class_c_percentage = (length_class_c / total_hours) * 100
-
-    # Create plots using Matplotlib
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
     labels = ['Class A', 'Class B', 'Class C']
@@ -128,3 +74,4 @@ if file_path is not None:
 
     # Display the plots using Streamlit
     st.pyplot(fig)
+
