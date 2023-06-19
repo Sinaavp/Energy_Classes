8b9de2d93ee4bd6b28261baca03eb3785f5b96da
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-06-19 18:23:18 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -70,47 +70,49 @@ if uploaded_file is not None:
         else:
             return 0
 
-    def comfort(df):
-        months = df['Month'].unique()
-        for month in months:
-            month_df = df[df['Month'] == month]
-            length_class_a = month_df['class_A'].sum()
-            length_class_b = month_df['class_B'].sum()
-            length_class_c = month_df['class_C'].sum()
-            total_hours = len(month_df)
-            discomfort_percentage = ((total_hours - length_class_c) / total_hours) * 100
-            comfort_percentage = (length_class_c / total_hours) * 100
-            class_a_percentage = (length_class_a / total_hours) * 100
-            class_b_percentage = (length_class_b / total_hours) * 100
-            class_c_percentage = (length_class_c / total_hours) * 100
+def comfort(df):
+    months = df['Month'].unique()
+    for month in months:
+        month_df = df[df['Month'] == month]
+        length_class_a = month_df['class_A'].sum()
+        length_class_b = month_df['class_B'].sum()
+        length_class_c = month_df['class_C'].sum()
+        total_hours = len(month_df)
+        discomfort_percentage = ((total_hours - length_class_c) / total_hours) * 100
+        comfort_percentage = (length_class_c / total_hours) * 100
+        class_a_percentage = (length_class_a / total_hours) * 100
+        class_b_percentage = (length_class_b / total_hours) * 100
+        class_c_percentage = (length_class_c / total_hours) * 100
 
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 7.5))
-            labels1 = ['Class A', 'Class B', 'Class C']
-            sizes1 = [class_a_percentage, class_b_percentage, class_c_percentage]
-            labels2 = ['Comfort', 'Discomfort']
-            sizes2 = [comfort_percentage, discomfort_percentage]
-            colors2 = ['green', 'red']
+        fig, axs = plt.subplots(1, 2, figsize=(15, 7.5))
+        ax1, ax2 = axs
 
-            if all(size != 0 for size in sizes1) and all(size != 0 for size in sizes2):
-                ax1.pie(sizes1, labels=None, autopct='%1.1f%%', startangle=90, pctdistance=0.85)
-                ax1.legend(labels1, loc='lower center')
-                ax1.axis('equal')
-                ax1.set_title('EN 15251 COMFORT HOURS - {}'.format(month))
-                centre_circle1 = plt.Circle((0, 0), 0.70, fc='white')
-                ax1.add_artist(centre_circle1)
-                ax2.pie(sizes2, labels=None, colors=colors2, autopct='%1.1f%%', startangle=90, pctdistance=0.85)
-                ax2.legend(labels2, loc='lower center')
-                ax2.axis('equal')
-                ax2.set_title('EN 15251 COMFORT VS DISCOMFORT - {}'.format(month))
-                centre_circle2 = plt.Circle((0, 0), 0.70, fc='white')
-                ax2.add_artist(centre_circle2)
-            else:
-                ax1.text(0.5, 0.5, 'NO COMFORT RANGE IN - {}'.format(month), horizontalalignment='center',
-                         verticalalignment='center', transform=ax1.transAxes)
-                ax2.text(0.5, 0.5, 'NO COMFORT RANGE IN - {}'.format(month), horizontalalignment='center',
-                         verticalalignment='center', transform=ax2.transAxes)
+        labels1 = ['Class A', 'Class B', 'Class C']
+        sizes1 = [class_a_percentage, class_b_percentage, class_c_percentage]
+        labels2 = ['Comfort', 'Discomfort']
+        sizes2 = [comfort_percentage, discomfort_percentage]
+        colors2 = ['green', 'red']
 
-            st.pyplot(fig)
+        if all(size != 0 for size in sizes1) and all(size != 0 for size in sizes2):
+            ax1.pie(sizes1, labels=None, autopct='%1.1f%%', startangle=90, pctdistance=0.85)
+            ax1.legend(labels1, loc='lower center')
+            ax1.axis('equal')
+            ax1.set_title('EN 15251 COMFORT HOURS - {}'.format(month))
+            centre_circle1 = plt.Circle((0, 0), 0.70, fc='white')
+            ax1.add_artist(centre_circle1)
+            ax2.pie(sizes2, labels=None, colors=colors2, autopct='%1.1f%%', startangle=90, pctdistance=0.85)
+            ax2.legend(labels2, loc='lower center')
+            ax2.axis('equal')
+            ax2.set_title('EN 15251 COMFORT VS DISCOMFORT - {}'.format(month))
+            centre_circle2 = plt.Circle((0, 0), 0.70, fc='white')
+            ax2.add_artist(centre_circle2)
+        else:
+            ax1.text(0.5, 0.5, 'NO COMFORT RANGE IN - {}'.format(month), horizontalalignment='center',
+                     verticalalignment='center', transform=ax1.transAxes)
+            ax2.text(0.5, 0.5, 'NO COMFORT RANGE IN - {}'.format(month), horizontalalignment='center',
+                     verticalalignment='center', transform=ax2.transAxes)
+
+        st.pyplot(fig)
 
     if options == "Comfort EN":
         if 'df' in locals():
