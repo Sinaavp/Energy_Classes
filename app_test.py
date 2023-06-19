3f051431e47b0321e1b71a91dce9f556ade56d1a
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-06-19 16:32:37 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 st.title("ENERGY LAB")
 st.sidebar.title("Navigation")
 options=st.sidebar.radio("pages", options=["Comfort EN", "Temperature", "Radiation", "Relative humidity", "Interior Temperature"])
-uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv", "txt"], default=None)
+uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv", "txt"])
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, delimiter='\t', header=[0, 1])
 
