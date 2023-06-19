a18264ceaa52674e6d043f7b16cf65571f37be0f
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-06-19 16:27:32 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -6,8 +6,8 @@ st.title("ENERGY LAB")
 st.sidebar.title("Navigation")
 options=st.sidebar.radio("pages", options=["Comfort EN", "Temperature", "Radiation", "Relative humidity", "Interior Temperature"])
 uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv", "txt"])
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, delimiter='\t', header=[0, 1])    
+df = pd.read_csv(uploaded_file, delimiter='\t', header=[0, 1]) if uploaded_file else None
+
 new_columns = [
     ('AirTemp', 'Min'),
     ('AirTemp', 'Average'),
