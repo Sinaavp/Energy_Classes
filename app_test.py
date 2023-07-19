bb9210396c6563bc75b0815dbe01d6dfaf9eecef
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-19 15:20:18 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -147,6 +147,18 @@ if uploaded_file is not None:
         if 'df' in locals():
             st.subheader("Temperature Line Graph")
             fig = px.line(df, x=df.index, y='AirTemp_Average')
+            fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all")
+                ])
+            )
+        )
             st.plotly_chart(fig)
         else:
             st.write("Please upload a file.")
