8dd0e3aa8ab0a7820dc979f1760121d7ed7d062b
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-23 19:16:18 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -118,7 +118,7 @@ if uploaded_file is not None:
                 ax1.set_title('EN 15251 COMFORT HOURS - {}'.format(month))
                 ax1.legend()
                 for i, v in enumerate([class_a_percentage, class_b_percentage, class_c_percentage]):
-                    ax1.text(v + 1, i, f'{v:.1f}%', color='black', va='center')
+                    ax1.text(v + 1, f'{v:.1f}%', color='black', va='center')
                 
 
                 # Stacked Bar Chart for Comfort and Discomfort
