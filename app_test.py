db5376b24217b5a82d8e8df6ffd933b95f24d31d
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-24 14:55:13 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -119,8 +119,8 @@ if uploaded_file is not None:
                 ax1.legend()
 
                 for bar, label in zip(bars, labels1):
-                    width = bar.get_width()
-                    ax1.annotate(f'{label}: {width:.1f}%', xy=(width, bar.get_y() + bar.get_height() ),
+                    width = bar.get_width()/2
+                    ax1.annotate(f'{label}: {width:.1f}%', xy=(width, bar.get_y() + bar.get_height()/2 ),
                                  xytext=(0, 0), textcoords='offset points', ha='center', va='center')
                     
                     
