1ff0415434a9f307e82d4bf595f210c0d0ec2e0a
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-24 16:42:19 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -111,7 +111,9 @@ if uploaded_file is not None:
             if all(size != 0 for size in [class_a_percentage, class_b_percentage, class_c_percentage]):
                 # Stacked Bar Chart for Class A, B, and C
                 labels1 = ['Class A', 'Class B', 'Class C']
-                bars1 = ax1.barh(labels1, [class_a_percentage, class_b_percentage, class_c_percentage], color=['blue', 'orange', 'green'])
+                bars = ax1.barh(idx, class_a_percentage, label='Class A', color='blue')
+                bars = ax1.barh(idx, class_b_percentage, left=class_a_percentage, label='Class B', color='orange')
+                bars = ax1.barh(idx, class_c_percentage, left=class_a_percentage + class_b_percentage, label='Class C', color='green')
                 ax1.set_xlabel('Comfort classes')
                 ax1.set_title('EN 15251 COMFORT HOURS - {}'.format(month))
                 ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=3) 
