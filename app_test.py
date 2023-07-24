2c055683b7c17a025b2cdbef1aa5ebec9eb3a9a7
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-24 14:20:01 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -120,7 +120,7 @@ if uploaded_file is not None:
 
                 # Annotating the bars with percentage values
                 for bar, label, value_a, value_b, value_c in zip(bars, labels1, [class_a_percentage], [class_b_percentage], [class_c_percentage]):
-                    x = bar.get_x() + bar.get_width()
+                    x = bar.get_x() + bar.get_width()  / 2
                     y = bar.get_y() + bar.get_height() / 2
                     ax1.text(x, y, f"{label}: {value_a:.2f}%", ha='left', va='center', color='black', fontweight='bold')
                     ax1.text(x, y, f"{label}: {value_b:.2f}%", ha='left', va='center', color='black', fontweight='bold')
