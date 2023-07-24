d5262053c087d1f7a2f9e4d2a82bde566ed0c352
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-24 14:10:13 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -119,7 +119,7 @@ if uploaded_file is not None:
                 ax1.legend()
 
                 # Annotating the bars with percentage values
-                for value in bars:
+                for bar, value in zip(bars, [class_a_percentage, class_b_percentage, class_c_percentage]):
                     ax1.text(value, bar.get_y() + bar.get_height() / 2, f"{value:.2f}%", ha='left', va='center', color='black', fontweight='bold')
 
                 
