ab4b93d2d25f63fb7d67454eaa88dfc20bf83169
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-25 16:59:14 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 from io import BytesIO
 
 st.title("ENERGY LAB")
