6935a74ee3e37b7e3722c36fff137db690ab06c2
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-07-25 16:57:29 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -171,6 +171,9 @@ if uploaded_file is not None:
             )
         )
             st.plotly_chart(fig)
+        if 'df' in locals():
+            fig2 = go.Figure(data=go.Heatmap(z=[y], x=df.index, y='AirTemp_Average', colorscale='RdBu_r'))
+            st.plotly_chart(fig2)
         else:
             st.write("Please upload a file.")
 
