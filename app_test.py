cc191eadbe734f472ce9d1c53b8692d3c4506ba8
Sinaavp <95223025+Sinaavp@users.noreply.github.com>
2023-08-03 18:16:43 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -240,17 +240,17 @@ if uploaded_file is not None:
             fig.update_xaxes(title_text='Date')
             fig.update_yaxes(title_text='Radiation')
             fig.update_xaxes(
-            rangeslider_visible=True,
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1, label="1m", step="month", stepmode="backward"),
-                    dict(count=6, label="6m", step="month", stepmode="backward"),
-                    dict(count=1, label="YTD", step="year", stepmode="todate"),
-                    dict(count=1, label="1y", step="year", stepmode="backward"),
-                    dict(step="all")
-                ])
+                rangeslider_visible=True,
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="YTD", step="year", stepmode="todate"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all")
+                    ])
+                )
             )
-        )
             st.plotly_chart(fig)
         if 'df' in locals():
             columns_to_check_duplicates = ['Date', 'Average_Hourly_Temp', 'Hour']
